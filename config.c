a89e1edf67f7d9e02145b276061f5d31c4cad821
Sean Groarke <sgroarke@gmail.com>
2011-07-11 07:59:56 +0000

    Test 2

--- config.c
+++ config.c
@@ -21,7 +21,7 @@
 #include "includes.h"
 #include "npd6.h"
 #include "npd6config.h"
-//## Test comment
+//## Test comment2
 
 //*******************************************************
 // Take supplied filename and open it, then parse the contents.
