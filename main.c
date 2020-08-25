9c3724be2897b08d460912049b58f38741ec788a
Thorsten Alteholz <debian@alteholz.de>
2020-08-25 21:56:26 +0200

    fix FTBFS with gcc10

--- main.c
+++ main.c
@@ -25,6 +25,46 @@
 #include "includes.h"
 #include "npd6.h"
 
+//*****************************************************************************
+// Globals
+//
+char            *pname;
+char            *paramName;
+int             sockpkt;
+int             debug;
+int             daemonize;
+FILE            *logFileFD;
+int             logging;
+char            configfile[FILENAME_MAX];
+FILE            *configFileFD;
+int             initialIFFlags;
+
+unsigned int    interfaceCount;         // Total number of interface/prefix combos
+// We dynaimcally size this at run-time
+struct  npd6Interface *interfaces;
+
+// Key behaviour
+int             naLinkOptFlag;      // From config file NPD6OPTFLAG
+int             nsIgnoreLocal;      // From config file NPD6LOCALIG
+int             naRouter;           // From config file NPD6ROUTERNA
+int             maxHops;            // From config file NPD6MAXHOPS
+int             collectTargets;     // From config file NPD6TARGETS
+
+// Target tree data structures etc
+void            *tRoot;
+int             tEntries;
+
+// Black/whitelisting data
+void            *lRoot;
+int             listType;
+int             listLog;            // From config file NPD6LISTLOG
+
+// Logging - various
+int             ralog;              // From config file NPD6RALOG
+
+// Error handling
+int		        pollErrorLimit;     // From config file
+
 char usage_str[] =
 {
     "\n"
